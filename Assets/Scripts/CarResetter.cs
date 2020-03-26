1389eaf922514b24c49882d3b63cdda707ed8ed7
Niels <389435@student.fontys.nl>
2020-03-26 14:49:20 +0100

    Added score + reset scripts
    Added a script to give score when jumping off the ramp. Also added a car reset script (press r to reset car to its original spawn position). Also made the score work in a ui canvas and finally added colliders to the ramp.

---
+++ Assets/Scripts/CarResetter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarResetter : MonoBehaviour
+{
+    private Vector3 spawnPosition;
+    private Quaternion spawnRotation;
+
+    void Start()
+    {
+        spawnPosition = gameObject.transform.position;
+        spawnRotation = gameObject.transform.rotation;
+    }
+
+    void Update()
+    {
+        checkReset();
+    }
+
+    void checkReset()
+    {
+        if(Input.GetKey(KeyCode.R))
+        {
+            gameObject.transform.position = spawnPosition;
+            gameObject.transform.rotation = spawnRotation;
+            gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Text stuntText;
+    [SerializeField] private float score;
+
+    private float points;
+    private float timeLeft;
+
+    void Start()
+    {
+        timeLeft = 0;
+    }
+
+    void Update()
+    {
+        if(timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+        }
+        else
+        {
+            stuntText.enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        stuntText.enabled = true;
+        timeLeft = 2;
+        stuntText.text = "Ramp jump! +" + score.ToString();
+        scoreText.text = "Score: " + (points += score).ToString();
+    }
+}
