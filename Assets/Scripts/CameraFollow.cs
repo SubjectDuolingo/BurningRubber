ae0a05acea8d48a96ee195e7a6b678c36727ea14
Matthijs van Spelde <I404906@fhict.nl>
2020-03-28 09:22:58 +0100

    Ignore

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Slerp(transform.position, desiredPosition, smoothSpeed);     
+        transform.position = smoothedPosition + (-target.transform.forward * offset.magnitude);
+        transform.LookAt(target);
+    }
+}
