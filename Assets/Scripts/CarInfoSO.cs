09e7e935b7b03f977a4ff9db16c704bb8607c393
Niels <389435@student.fontys.nl>
2020-04-09 12:34:07 +0200

    Saving selected car
    Changed the car selection menu so that a car body can be selected (for now its just a body). The sphere/capsule wont work in the scriptable object as its not a gameobject. Car bodies can be selected and will transfer to the scriptable object without problems.

---
+++ Assets/Scripts/CarInfoSO.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CarInfoSO")]
+public class CarInfoSO : ScriptableObject
+{
+    public GameObject selectedCar;
+
+    public void SelectNewCar(GameObject car)
+    {
+        selectedCar = car;
+    }
+
+    public GameObject GetSelectedCar()
+    {
+        return selectedCar;
+    }
+}
---
+++ Assets/Scripts/CarSelector.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSelector : MonoBehaviour
+{
+    public CarInfoSO carObject;
+    public List<GameObject> carList = new List<GameObject>();
+    public int selectedCar = 0;
+    public int totalCars;
+
+    private void Start()
+    {
+        totalCars = (carList.Count - 1);
+    }
+
+    public void RightButtonClicked()
+    {
+        gameObject.transform.Rotate(new Vector3(0, -90, 0));
+        if(selectedCar < totalCars)
+        {
+            selectedCar++;
+        }
+        else
+        {
+            selectedCar = 0;
+        }
+    }
+
+    public void LeftButtonClicked()
+    {
+        gameObject.transform.Rotate(new Vector3(0, 90, 0));
+        if (selectedCar > 1)
+        {
+            selectedCar--;
+        }
+        else
+        {
+            selectedCar = totalCars;
+        }
+    }
+
+    public void SelectCarButtonClicked()
+    {
+        carObject.SelectNewCar(carList[selectedCar]);
+    }
+}
